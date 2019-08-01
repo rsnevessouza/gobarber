a14854a4424a1b002e7420a53f092f7967e9cdea
Suporte <suporte@oficinadasfinancas.com.br>
2019-08-01 16:48:54 -0300

    fix: removido bug no gerar hash para usuario

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,5 +1,5 @@
 import Sequelize, { Model } from 'sequelize';
-import bcrypt from 'bcryptjs'
+import bcrypt from 'bcryptjs';
 
 class User extends Model {
   static init(sequelize) {
@@ -16,8 +16,8 @@ class User extends Model {
       }
     );
 
-    this.addHook('beforeSave', (user) => {
-      if(user.password) {
+    this.addHook('beforeSave', async user => {
+      if (user.password) {
         user.password_hash = await bcrypt.hash(user.password, 8);
       }
     });
