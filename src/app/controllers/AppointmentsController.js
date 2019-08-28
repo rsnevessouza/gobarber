aef8203f07373811dacc488e4358cbb942118802
Home MacOS <homemacos@Homes-iMac.local>
2019-08-28 16:07:52 -0300

    feat: Configura fila de emails com Redis

--- src/app/controllers/AppointmentsController.js
+++ src/app/controllers/AppointmentsController.js
@@ -6,7 +6,8 @@ import User from '../models/User';
 import File from '../models/File';
 import Notification from '../schemas/Notification';
 
-import Mail from '../../lib/Mail';
+import Queue from '../../lib/Queue';
+import CancellationMail from '../jobs/CancellationMail';
 
 class AppointmentsController {
   async index(req, res) {
@@ -113,12 +114,12 @@ class AppointmentsController {
         {
           model: User,
           as: 'provider',
-          atributes: ['name', 'email'],
+          attributes: ['name', 'email'],
         },
         {
           model: User,
           as: 'user',
-          atributes: ['name'],
+          attributes: ['name'],
         },
       ],
     });
@@ -141,18 +142,7 @@ class AppointmentsController {
 
     await appointment.save();
 
-    await Mail.sendMail({
-      to: `${appointment.provider.name} <${appointment.provider.email}>`,
-      subject: 'Você tem um novo cancelamento',
-      template: 'cancellation',
-      context: {
-        provider: appointment.provider.name,
-        user: appointment.user.name,
-        date: format(appointment.date, "'dia' dd 'de' MMMM', às' H:mm'h'", {
-          locale: pt,
-        }),
-      },
-    });
+    await Queue.add(CancellationMail.key, { appointment });
 
     return res.json(appointment);
   }
